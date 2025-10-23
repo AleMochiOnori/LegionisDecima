43d790ca06442de0c6485160f3e292a38f86fc50
Alessandro Mochi Onori <wink@MacBook-Pro-di-Alessandro.local>
2025-10-23 16:48:49 +0200

    ciao

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -1,48 +1,41 @@
 import "./Footer.css";
 import logo from "../../assets/Logo_Combat.jpg"
+import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer className="footer-container">
       <div className="footer-content">
-        
-        {/* LOGO */}
         <div className="footer-logo">
           <img src={logo} alt="Torrino Muay Thai" />
           <h2 className="textSize">Legionis Decima Torrino</h2>
           <p className="textSize">Passione, Disciplina, Rispetto</p>
         </div>
-
-        {/* CONTATTI */}
         <div className="footer-section">
           <h3 className="textSize">Contatti</h3>
-
-          <p className="textSize">📞 Torrino Sporting Center: 534345345345</p>
-          <p className="textSize">📞 Maestro Alessio Marabucci: 2342342345</p>
-          <p className="textSize">📞 Maestro Matteo Chiavolini: 234234253452</p>
+          <p className="textSize">📞 Torrino Sporting Center: 065295884</p>
+          <p className="textSize">📞 Maestro Alessio Marabucci: +39 328 925 3150</p>
+          <p className="textSize">📞 Istruttore Matteo Chiavolini: 234234253452</p>
         </div>
-
-        {/* LINK UTILI */}
         <div className="footer-section">
           <h3 className="textSize">Link Utili</h3>
-          <ul >
-            <li><a className="textSize" href="#">Home</a></li>
-            <li><a className="textSize" href="#">Corsi</a></li>
-            <li><a className="textSize" href="#">Staff</a></li>
-            <li><a className="textSize" href="#">Contatti</a></li>
+          <ul>
+            <li><Link to={"/"}>Home</Link></li>
+            <li><Link to={"/courses"}>Corsi</Link></li>
+            <li><Link to={"/aboutUs"}>Staff</Link></li>
+            <li><Link to={"/contacts"}>Contatti</Link></li>
+            <li><Link to={"/privateLesson"}>Lezione Privata</Link></li>
+            <li><Link to={"/rating"}>Recensioni</Link></li>
           </ul>
         </div>
-
-        {/* SOCIAL */}
         <div className="footer-section">
           <h3 className="textSize">Seguici</h3>
           <div className="social-icons">
-            <a  href="#" className="instagram textSize">Instagram</a>
-            <a href="#" className="facebook textSize">Facebook</a>
-            <a href="#" className="youtube textSize">YouTube</a>
+            <a  href="https://www.instagram.com/legionis_decima_torrino/" target="_blank" className="instagram textSize">Instagram</a>
+            <a href="#" target="_blank" className="facebook textSize">Facebook</a>
+            <a href="#" target="_blank" className="youtube textSize">YouTube</a>
           </div>
         </div>
       </div>
-
       <div className="footer-bottom">
         <p>© 2025 Legionis Decima Torrino| Tutti i diritti riservati</p>
       </div>
