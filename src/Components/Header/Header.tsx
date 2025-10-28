f36febceddbaf100c3b2b83da7b076b6bb38f05d
Alessandro Mochi Onori <wink@MacBook-Pro-di-Alessandro.local>
2025-10-28 12:09:51 +0100

    hamburger menu

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,7 +1,10 @@
 import "./Header.css"
 import { Link } from "react-router-dom";
 import logoCombat from "../../assets/Logo_Combat.jpg"
+import { useState } from "react";
+import Hamburger from 'hamburger-react'
 const Header = () => {
+    const [isOpen, setOpen] = useState(false)
     return (
         <>
             <div className="Header-Container">
@@ -9,7 +12,7 @@ const Header = () => {
                     <Link to={`/`}>
                         <img className="logoCombat" src={logoCombat} alt="" />
                     </Link>
-                    <h2>Legionis Decima Torrino</h2>
+                    <h2 className="titleHeader">Legionis Decima Torrino</h2>
                 </div>
                 <div className="Right-Header-Content">
                       <Link to={`/`}>
@@ -36,15 +39,24 @@ const Header = () => {
                          <p style={{marginLeft : "7px" , marginRight : "7px"}}>/</p>
                         <Link to={`/register`}><p>Registrati</p></Link>
                     </div>
-                    
                    
-
                 </div>
-                
+                    <div className="hamburger-menu">
+                        <Hamburger toggled={isOpen} toggle={setOpen} />
+                        
+                    </div>
             </div>
-
-
-
+            <div className={`mobile-sidebar ${isOpen ? "open" : "whited"}`}>
+                <button className="close-sidebar" onClick={() => setOpen(false)}>✕</button>
+                <Link to={`/`} onClick={() => setOpen(false)}>Home</Link>
+                <Link to={`/aboutUs`} onClick={() => setOpen(false)}>Staff</Link>
+                <Link to={`/courses`} onClick={() => setOpen(false)}>Orari corsi</Link>
+                <Link to={`/contacts`} onClick={() => setOpen(false)}>Contatti</Link>
+                <Link to={`/privateLesson`} onClick={() => setOpen(false)}>Lezione privata</Link>
+                <Link to={`/rating`} onClick={() => setOpen(false)}>Recensioni</Link>
+                <Link to={`/Login`} onClick={() => setOpen(false)}>Accedi</Link>
+                <Link to={`/register`} onClick={() => setOpen(false)}>Registrati</Link>
+            </div>
         </>
     )
 }; export default Header;
